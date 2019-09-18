83061e3f22d6c510f438c448d0fc32f5a614490d
xiongz <a824440331@gmail.com>
2019-09-18 18:17:31 +0800

    update code

---
+++ Modules/UserAccess/CompanyName.MyMeetings.Modules.UserAccess.Application/Authentication/PasswordManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CompanyName.MyMeetings.Modules.UserAccess.Application.Authentication
+{
+    public class PasswordManager
+    {
+        public static string HashPassword(string password)
+        {
+            byte[] salt;
+            byte[] buffer2;
+            if (password == null)
+            {
+                throw new ArgumentNullException(nameof(password));
+            }
+
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
+            {
+                salt = bytes.Salt;
+                buffer2 = bytes.GetBytes(0x20);
+            }
+
+            byte[] dst = new byte[0x31];
+            return String.Empty;
+        }
+    }
+}
