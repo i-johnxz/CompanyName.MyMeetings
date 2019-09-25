06a55195c651d776cb52ec7fbf44b8ae8b5c0ac3
xiongz <a824440331@gmail.com>
2019-09-25 18:00:02 +0800

    update code

--- API/CompanyName.MyMeetings.API/Startup.cs
+++ API/CompanyName.MyMeetings.API/Startup.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using CompanyName.MyMeetings.API.Configuration.Authorization;
